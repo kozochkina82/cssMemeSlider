77742f81ed8f1e645cb15db3565f882b491e7050
kozochkina82 <kozochkina82@yandex.ru>
2024-09-15 04:06:24 +0500

    feat: make pagination (Sun, September 15, 2024 04:06:24 AM)

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
+document.querySelector('.first-point').onclick = function() {selectButtonOne()};
+function selectButtonOne(){
+    document.querySelector('.first-point').classList.add('active');
+    document.querySelector('.second-point').classList.remove('active');
+    document.querySelector('.third-point').classList.remove('active');
+    document.querySelector('.fourth-point').classList.remove('active');
+    document.querySelector('.first-slide').classList.add('active');
+    document.querySelector('.second-slide').classList.remove('active');
+    document.querySelector('.third-slide').classList.remove('active');
+    document.querySelector('.fourth-slide').classList.remove('active');
+};
+
+document.querySelector('.second-point').onclick = function() {selectButtonTwo()};
+function selectButtonTwo(){
+    document.querySelector('.first-point').classList.remove('active');
+    document.querySelector('.second-point').classList.add('active');
+    document.querySelector('.third-point').classList.remove('active');
+    document.querySelector('.fourth-point').classList.remove('active');
+    document.querySelector('.first-slide').classList.remove('active');
+    document.querySelector('.second-slide').classList.add('active');
+    document.querySelector('.third-slide').classList.remove('active');
+    document.querySelector('.fourth-slide').classList.remove('active');
+};
+
+document.querySelector('.third-point').onclick = function() {selectButtonThree()};
+function selectButtonThree(){
+    document.querySelector('.first-point').classList.remove('active');
+    document.querySelector('.second-point').classList.remove('active');
+    document.querySelector('.third-point').classList.add('active');
+    document.querySelector('.fourth-point').classList.remove('active');
+};
+document.querySelector('.fourth-point').onclick = function() {selectButtonFour()};
+function selectButtonFour(){
+    document.querySelector('.first-point').classList.remove('active');
+    document.querySelector('.second-point').classList.remove('active');
+    document.querySelector('.third-point').classList.remove('active');
+    document.querySelector('.fourth-point').classList.add('active');
+};
 
